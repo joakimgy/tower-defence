0d6915b4c7ffde4ba2024584fab4b9b16b719d9a
Joakim Gyllenskepp <jockegy@gmail.com>
2021-06-12 10:59:53 +0200

    Improve resolution and tile size

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,7 +1,7 @@
 object Config {
     const val enemiesToSpawn = 3
     const val buildingBlocksPerRound = 5
-    
+
     object Screen {
         const val viewportWidth = 1280f
         const val viewportHeight = 720f
@@ -9,8 +9,8 @@ object Config {
 
     const val MAP_SIZE_X = 23
     const val MAP_SIZE_Y = 13
-    const val TILE_SIZE = 32f
-    const val FONT_SIZE = 50
+    const val TILE_SIZE = 50f
+    const val FONT_SIZE = 30
 
 
     object Player {
